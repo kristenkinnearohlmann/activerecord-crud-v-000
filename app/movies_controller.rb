4ce1e6ffee4e8f0d81c4964a96ccb8d67536432a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-07 22:19:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,7 +32,8 @@ def can_be_created_in_a_block(args = {})
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.title = args["title"] || 'Home Alone'
+    m.release_date = args["release_date"] || 1990
   end
 end
 
