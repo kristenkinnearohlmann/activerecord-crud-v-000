4d90b584f9bc44a9e325291ed9de6ab788746f89
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-08 02:45:37 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -93,7 +93,6 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  binding.pry
   Movie.delete(movie.id)
 end
 
